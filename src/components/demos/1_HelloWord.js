d28cde2fec37ccda0513961259abc205f1a7b93e
maoguijun <guijun.mao@loncus.com>
2018-06-20 19:08:47 +0800

    no message

--- src/components/demos/1_HelloWord.js
+++ src/components/demos/1_HelloWord.js
@@ -1,4 +1,4 @@
 import React from "react";
 /** Hello World */
-const HelloWord = props => <h1>Hello, world!</h1>;
+const HelloWord = props => <h1>Hello, world!, 您好！</h1>;
 export default HelloWord;
