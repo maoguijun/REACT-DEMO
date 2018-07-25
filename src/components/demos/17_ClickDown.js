6efafb23d830ede5b7971b826cc8e3f61aad57c1
maoguijun <guijun.mao@loncus.com>
2018-07-25 15:19:47 +0800

    下载

--- src/components/demos/17_ClickDown.js
+++ src/components/demos/17_ClickDown.js
@@ -13,10 +13,18 @@ class ClickDown extends Component {
         </a>
         <img src={"/download/it/u=4135158290,2541209692&fm=27&gp=0.jpg"} />
         <a
-          href={"/download/1c63715d-c9c4-4a7d-99e6-aa69809f1ce3.pdf"}
-          download="深入react技术栈.pdf"
+          href={
+            "http://img2.imgtn.bdimg.com/it/u=4135158290,2541209692&fm=27&gp=0.jpg"
+          }
+          download="图片"
         >
-          点我下载
+          点我下载(直接通过百度图库的地址下载)
+        </a>
+        <a
+          href={"/download/u=4135158290,2541209692&fm=27&gp=0.jpg"}
+          download="图片"
+        >
+          点我下载(通过自己nginx反向代理下载)
         </a>
       </div>
     );
