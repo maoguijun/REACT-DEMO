d6d6a0b0e6a9f5c3a835fae532ca94dd11b766a2
maoguijun <guijun.mao@loncus.com>
2018-07-25 15:15:30 +0800

    no message

--- src/components/demos/17_ClickDown.js
+++ src/components/demos/17_ClickDown.js
@@ -11,6 +11,7 @@ class ClickDown extends Component {
         >
           深入react技术栈.pdf
         </a>
+        <img src={"/download/it/u=4135158290,2541209692&fm=27&gp=0.jpg"} />
         <a
           href={"/download/1c63715d-c9c4-4a7d-99e6-aa69809f1ce3.pdf"}
           download="深入react技术栈.pdf"
