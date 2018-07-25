400e4cfd7f9d36a7028a9a0fcd6f9531a8cf80b4
maoguijun <guijun.mao@loncus.com>
2018-07-25 13:58:00 +0800

    no message

--- src/components/demos/17_ClickDown.js
+++ src/components/demos/17_ClickDown.js
@@ -1,13 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 
 class ClickDown extends Component {
   state = {};
   render() {
     return (
       <a
-        href={
-          "http://youplustest.oss-cn-shanghai.aliyuncs.com/1c63715d-c9c4-4a7d-99e6-aa69809f1ce3.pdf"
-        }
+        href={"/download/1c63715d-c9c4-4a7d-99e6-aa69809f1ce3.pdf"}
         download="深入react技术栈.pdf"
       >
         点我下载
